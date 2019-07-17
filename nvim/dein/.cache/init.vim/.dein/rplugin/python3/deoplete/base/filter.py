430cceb9dcdc1c29bdda1ba908b8b304c10ae501
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2019-07-17 12:43:00 +0900

    一旦コミット

---
+++ nvim/dein/.cache/init.vim/.dein/rplugin/python3/deoplete/base/filter.py
@@ -0,0 +1,36 @@
+# ============================================================================
+# FILE: filter.py
+# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
+# License: MIT license
+# ============================================================================
+
+from abc import abstractmethod
+from deoplete.logger import LoggingMixin
+from deoplete.util import error_vim
+
+
+class Base(LoggingMixin):
+
+    def __init__(self, vim):
+        self.vim = vim
+        self.name = 'base'
+        self.description = ''
+
+    def on_event(self, context):
+        pass
+
+    def get_var(self, var_name):
+        custom_vars = self.vim.call(
+            'deoplete#custom#_get_filter', self.name)
+        if var_name in custom_vars:
+            return custom_vars[var_name]
+        if var_name in self.vars:
+            return self.vars[var_name]
+        return None
+
+    @abstractmethod
+    def filter(self, context):
+        pass
+
+    def print_error(self, expr):
+        error_vim(self.vim, expr)
