fbc81433b9f3b880beed26c9445f84ad842c67ea
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2026-02-09 23:36:44 +0900

    anyenvのパス修正

--- bashrc.d/01-path.bash
+++ bashrc.d/01-path.bash
@@ -1,5 +1,6 @@
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
+if command -v anyenv >/dev/null 2>&1; then
+  eval "$(anyenv init -)"
+fi
 
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/unzip/bin:$PATH"
