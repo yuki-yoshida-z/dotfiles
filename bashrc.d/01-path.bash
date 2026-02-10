affc71786ece36a9ab63e3e006f4acdac05d855f
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2026-02-10 11:17:04 +0900

    レビュー修正

--- bashrc.d/01-path.bash
+++ bashrc.d/01-path.bash
@@ -1,8 +1,8 @@
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/opt/unzip/bin:$PATH"
+
 if command -v anyenv >/dev/null 2>&1; then
   eval "$(anyenv init -)"
 fi
-
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/opt/unzip/bin:$PATH"
 export GOPATH=$(go env GOPATH)
 export PATH="$PATH:$GOPATH/bin"
